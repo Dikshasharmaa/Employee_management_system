dd8b2ab6e83e243fd7ceb8db37c06e78995d700b
Diksha Sharma <diksha66sharma78@gmail.com>
2025-06-12 17:00:42 -0400

    Created getters setters

--- springboot-backend/src/main/java/net/javaguides/springboot_backend/model/Employee.java
+++ springboot-backend/src/main/java/net/javaguides/springboot_backend/model/Employee.java
@@ -1,4 +1,37 @@
 package net.javaguides.springboot_backend.model;
 
 public class Employee {
+    private long Id;
+    private String firstName;
+    private String lastName;
+    private String emailID;
+
+    public long getId(){
+        return Id;
+    }
+
+    public void setId(long id) {
+        this.Id = id;
+    }
+    public String getFirstName(){
+        return firstName;
+    }
+    public void setFirstName(String firstName){
+        this.firstName = firstName;
+    }
+    public String getLastName(){
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmailID() {
+        return emailID;
+    }
+
+    public void setEmailID(String emailID) {
+        this.emailID = emailID;
+    }
 }
