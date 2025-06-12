9f6bd2bab790e7f5b15d9b5f4ca3d3d6548d5335
Diksha Sharma <diksha66sharma78@gmail.com>
2025-06-12 19:18:09 -0400

    Completed the Jpa entity

--- springboot-backend/src/main/java/net/javaguides/springboot_backend/model/Employee.java
+++ springboot-backend/src/main/java/net/javaguides/springboot_backend/model/Employee.java
@@ -1,10 +1,23 @@
 package net.javaguides.springboot_backend.model;
 
+import jakarta.persistence.*;
+
+@Entity //Maps the Java class to a database table.
+@Table(name="employees")
 public class Employee {
+    @Id //marks id as the primary key
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long Id;
+    @Column(name = "first_name")
     private String firstName;
+    @Column(name = "last_name")
     private String lastName;
+    @Column(name = "email_id")
     private String emailID;
+
+    public Employee(){
+
+    }
 
     public Employee(String firstName, String lastName, String emailID){
         super();
