bae89f58552e4ea11e86399de4ecfccb8f73b546
Diksha Sharma <diksha66sharma78@gmail.com>
2025-07-10 13:55:04 -0400

    Added cors config file

--- springboot-backend/src/main/java/net/javaguides/springboot_backend/controller/EmployeeController.java
+++ springboot-backend/src/main/java/net/javaguides/springboot_backend/controller/EmployeeController.java
@@ -1,10 +1,16 @@
 package net.javaguides.springboot_backend.controller;
 
+import net.javaguides.springboot_backend.exception.ResourceNotFoundException;
 import net.javaguides.springboot_backend.model.Employee;
 import net.javaguides.springboot_backend.repository.EmployeeRepository;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+// import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -24,8 +30,37 @@ public class EmployeeController {
     }
 
     //Creating employees
+    // @CrossOrigin(origins = "http://localhost:4200")
     @PostMapping("/employees")
     public Employee createEmployee(@RequestBody Employee employee){
         return employeeRepository.save(employee);
+    }
+
+    //get employee by ID
+    @GetMapping("/employees/{id}")
+    public ResponseEntity<Employee> getEmployeesById(@PathVariable Long id){
+
+        Employee employee = employeeRepository.findById(id)
+        .orElseThrow(()->new ResourceNotFoundException("Employee do not exist with id :"+id));
+
+        return  ResponseEntity.ok(employee);
+    }
+
+    //Update employee 
+    @PutMapping("/employees/{id}")
+    public ResponseEntity<Employee> 
+    updateEmployee(@PathVariable long id, 
+    @RequestBody Employee employeeDetails){
+        Employee employee = employeeRepository.findById(id)
+        .orElseThrow(()->new ResourceNotFoundException("Employee do not exist with id :"+id));
+
+        employee.setFirstName(employeeDetails.getFirstName());
+        employee.setLastName(employeeDetails.getLastName());
+        employee.setEmailID(employeeDetails.getEmailID());
+
+        Employee updatedEmployee = employeeRepository.save(employee);
+        return ResponseEntity.ok(updatedEmployee);
+
+
     }
 }
